bf3419d7faeafa7ba99859ec2cec20dfba3ac22b
enochfunso <enochfunsho58@gmail.com>
2026-06-09 16:39:54 -0700

    main.py

--- Lagos-shuttle-tracker/main.py
+++ Lagos-shuttle-tracker/main.py
@@ -134,7 +134,7 @@ async def simulator():
 async def startup():
     asyncio.create_task(simulator())
 
-import os
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8765))
     uvicorn.run(app, host="0.0.0.0", port=port)
